2774950300fe7789ba4d18d236faf56c3ff97f21
mas <mas154572@gmail.com>
2024-09-02 21:24:30 -0400

    Made a button that activates the quote function

--- script.js
+++ script.js
@@ -3,14 +3,24 @@
 // selecting elements
 const quote = document.querySelector("[data-quote]");
 const author = document.querySelector("[data-author");
+const quoteButton = document.querySelector("[data-quote-button]");
+
+// adding event listener to button
+
+quoteButton.addEventListener("click", quoteGenerator);
 
 // calling api
 
 async function quoteGenerator() {
-  const loadQuote = await fetch("https://api.quotable.io/random");
-  console.log(loadQuote);
-  const loadedQuote = await loadQuote.json();
-  console.log(loadedQuote);
+  const quoteResponse = await fetch("https://api.quotable.io/random");
+  const quoteJSON = await quoteResponse.json();
+  const quoteAuthor = quoteJSON.author;
+  const quoteContent = quoteJSON.content;
+
+  //   setting content
+  quote.textContent = quoteContent;
+  author.textContent = quoteAuthor;
 }
 
+// calling function when page loads
 quoteGenerator();
